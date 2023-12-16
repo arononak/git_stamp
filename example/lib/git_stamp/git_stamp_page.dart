ea5a85a6708c6efde6c27a660ea5d0d2df9b8018
Aron Onak <arononak@gmail.com>
2023-12-16 13:36:12 +0100

    Example project update

--- example/lib/git_stamp/git_stamp_page.dart
+++ example/lib/git_stamp/git_stamp_page.dart
@@ -22,13 +22,11 @@ class GitStampPage extends StatelessWidget {
       appBar: AppBar(
         title: Row(
           children: [
-            Text('Git Stamp'),
+            Text('Git Stamp - commits:'),
             SizedBox(width: 8),
             CircleAvatar(
               child: Text(GitStampCommit.commitList.length.toString()),
             ),
-            SizedBox(width: 8),
-            Text('commits'),
           ],
         ),
       ),
@@ -68,6 +66,8 @@ class GitStampPage extends StatelessWidget {
                       ),
                     ],
                   ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
                 ),
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
