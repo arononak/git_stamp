f82b44c0d3937cd5e1973a73e2c129c836f21575
Aron Onak <arononak@gmail.com>
2024-02-14 00:30:24 +0100

    generate.dart refactor

---
+++ bin/generated_files/generated_git_stamp_commit.dart
@@ -0,0 +1,32 @@
+const generatedGitStampCommit = '''
+import 'dart:convert';
+
+import 'git_stamp_json_output.dart';
+
+class GitStampCommit {
+  final String hash;
+  final String subject;
+  final String date;
+  final String authorName;
+  final String authorEmail;
+
+  GitStampCommit({
+    required this.hash,
+    required this.subject,
+    required this.date,
+    required this.authorName,
+    required this.authorEmail,
+  });
+
+  factory GitStampCommit.fromJson(Map<String, dynamic> json) => GitStampCommit(
+        hash: json['hash'],
+        subject: json['subject'],
+        date: json['date'],
+        authorName: json['authorName'],
+        authorEmail: json['authorEmail'],
+      );
+
+  static List<GitStampCommit> get commitList =>
+      json.decode(jsonOutput).map<GitStampCommit>((json) => GitStampCommit.fromJson(json)).toList();
+}
+''';
