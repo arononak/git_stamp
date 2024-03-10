c300575cc89bc37c7d944554965da12fca3b6331
Aron Onak <arononak@gmail.com>
2024-03-10 09:03:02 +0100

    Example project new example page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:example/git_stamp/git_stamp_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,15 +24,36 @@ class ExamplePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Example Page'),
-        actions: [
-          IconButton(
-            onPressed: () => showGitStampPage(context: context),
-            icon: const Icon(Icons.navigate_next),
+        title: Text('Settings Page'),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text('Open Source Licenses'),
+            leading: const Icon(Icons.gavel),
+            onTap: () => showLicensePage(context: context),
           ),
+          ListTile(
+            title: Text('Version: 1.0.0'),
+            leading: const Icon(Icons.info),
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('1.0.0'),
+                  duration: const Duration(seconds: 2),
+                ),
+              );
+            },
+          ),
+          if (kDebugMode) ...[
+            ListTile(
+              title: const Text('Git Stamp'),
+              leading: const Icon(Icons.commit),
+              onTap: () => showGitStampPage(context: context),
+            ),
+          ],
         ],
       ),
-      body: Center(child: Text('Aron Soft sp. z o.o.')),
     );
   }
 }
