9e58d463b8de5bc144f5d82f3ec51fd80b6ab6f4
Aron Onak <arononak@gmail.com>
2024-08-12 15:05:07 +0200

    Generator - Formated file sizes

--- bin/src/git_stamp_file.dart
+++ bin/src/git_stamp_file.dart
@@ -7,23 +7,23 @@ abstract class GitStampFile {
   String filename();
   String content();
 
-  String getFileSize({int decimals = 2}) {
-    const suffixes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    final bytes = File(filename()).lengthSync();
+  String getFileSize({int decimals = 1}) {
+    const suffixes = ["B ", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    final fileSize = File(filename()).lengthSync();
 
-    if (bytes <= 0) {
+    if (fileSize <= 0) {
       return "0 B";
     }
 
-    final i = (log(bytes) / log(1024)).floor();
+    final i = (log(fileSize) / log(1024)).floor();
 
-    return '${(bytes / pow(1024, i)).toStringAsFixed(decimals)} ${suffixes[i]}';
+    return '${(fileSize / pow(1024, i)).toStringAsFixed(decimals)} ${suffixes[i]}';
   }
 
   void generate() {
     File(filename()).writeAsStringSync(content());
-    GitStampLogger()
-        .logger
-        .info('Generated - ${getFileSize()} \t ${filename()}');
+    
+    final text = 'Generated ${getFileSize().padLeft(12)}        ${filename()}';
+    GitStampLogger().logger.info(text);
   }
 }
